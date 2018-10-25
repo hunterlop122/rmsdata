f2b014038464605e7123567a4e4d662d51d8b064
Hunter Lopez <hunterlopez@Hunters-MacBook-Pro.local>
2018-10-25 10:55:41 -0400

    changed route file with root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'dancers/isadancer'
       resources :dancers
+  root to: 'dancers#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
